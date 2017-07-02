11ec9ca7de29009c591b2888398a0291d72bc7b9
Marcel <Marcel@Marcel_Windows8>
2017-07-02 22:12:09 +0200

    Batch-Dateien hinzugefügt - Start und Kompilieren unter Windows per Doppelklick!

---
+++ ReferenzProjektJavaSE7_Compile.bat
@@ -0,0 +1,6 @@
+rem [javac -d ..\..\..\..\..\build\classes . src.de.marcelhuber.referenzprojektjavase7.controller.GrundschulVerwaltungStartMVC.java]
+rem [Frage: Was macht der .?]
+cd src
+javac -d ..\build\classes de\marcelhuber\referenzprojektjavase7\controller\GrundschulVerwaltungStartMVC.java 
+rem [pause]
+cmd
---
+++ ReferenzProjektJavaSE7_Start.bat
@@ -0,0 +1,6 @@
+rem [cmd]
+rem [rem [cmd] bedeutet, dass das cmd nur ein Kommentar ist]
+cd build\classes
+java de.marcelhuber.referenzprojektjavase7.controller.GrundschulVerwaltungStartMVC
+cmd
+rem [pause]
