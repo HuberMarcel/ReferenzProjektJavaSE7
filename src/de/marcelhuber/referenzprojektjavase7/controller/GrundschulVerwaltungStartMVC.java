5959ccb656f6edd4e7f9a9fb16b29eef3dc17adb
Marcel <Marcel@Marcel_Windows8>
2017-07-06 09:43:53 +0200

    kleine Kommentare an der GUI ergänzt...

--- src/de/marcelhuber/referenzprojektjavase7/controller/GrundschulVerwaltungStartMVC.java
+++ src/de/marcelhuber/referenzprojektjavase7/controller/GrundschulVerwaltungStartMVC.java
@@ -1,6 +1,7 @@
 package de.marcelhuber.referenzprojektjavase7.controller;
 // Hinweis: Nachfragen, ob diese "Starklasse" in ein eigenes Package gehört
 //          bzw. ausgelagert bzgl. der M-V-C packages...
+//          nach Rückfrage: Kann im Package für den Controller bleiben
 
 import de.marcelhuber.referenzprojektjavase7.model.GrundschulVerwaltungGesamtModel;
 import de.marcelhuber.referenzprojektjavase7.view.*;
