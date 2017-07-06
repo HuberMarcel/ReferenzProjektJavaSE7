256bdb1a37fb01bed5ab6834beec20f3558701b5
Marcel <Marcel@Marcel_Windows8>
2017-07-06 12:04:35 +0200

    Enum für verschiedene Login-Typen am Bauen...

--- src/de/marcelhuber/referenzprojektjavase7/view/EnumLoginPersonen.java
+++ src/de/marcelhuber/referenzprojektjavase7/view/EnumLoginPersonen.java
@@ -5,8 +5,20 @@ package de.marcelhuber.referenzprojektjavase7.view;
  * @author Marcel Huber
  */
 public enum EnumLoginPersonen {
-    DIREKTOR, KONTAKTPERSON, EXTERN,;
+
+    DIREKTOR("Direktor"),
+    KONTAKTPERSON("Kontaktperson"),
+    EXTERN("Extern"),;
     private boolean loginOK;
+    private String loginTyp;
+
+    private EnumLoginPersonen(String loginTyp) {
+        this.loginTyp = loginTyp;
+    }
+
+    public String getLoginTyp() {
+        return loginTyp;
+    }
 
     public boolean isLoginOK() {
         return loginOK;
