19d00e02915542d54f678424954d00b3b623ebca
Marcel <Marcel@Marcel_Windows8>
2017-07-10 12:01:38 +0200

    batch-Datei für's Kompilieren angepasst... aber vermutlich immer noch ein Problem mit dem MySQL-Connector

--- ReferenzProjektJavaSE7_Compile.bat
+++ ReferenzProjektJavaSE7_Compile.bat
@@ -2,5 +2,7 @@ rem [javac -d ..\..\..\..\..\build\classes . src.de.marcelhuber.referenzprojektj
 rem [Frage: Was macht der .?]
 cd src
 javac -d ..\build\classes de\marcelhuber\referenzprojektjavase7\controller\GrundschulVerwaltungStartMVC.java
+rem [jar -cvfm ..\dist\ReferenzProjektJavaSE7.jar .\..\myManifest.mf de\marcelhuber\referenzprojektjavase7\controller\GrundschulVerwaltungStartMVC.java]
+jar -cvfm ..\dist\ReferenzProjektJavaSE7.jar .\..\myManifest.mf de\marcelhuber\referenzprojektjavase7\controller\GrundschulVerwaltungStartMVC.java
 rem [pause]
 cmd
