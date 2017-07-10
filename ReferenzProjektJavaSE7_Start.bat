e703a1cbaf4bee354bc9d5faac5f827ad5280dd3
Marcel <Marcel@Marcel_Windows8>
2017-07-10 09:26:35 +0200

    Anpassung des Batch-Files

--- ReferenzProjektJavaSE7_Start.bat
+++ ReferenzProjektJavaSE7_Start.bat
@@ -1,6 +1,7 @@
 rem [cmd]
 rem [rem [cmd] bedeutet, dass das cmd nur ein Kommentar ist]
 rem [java -cp .\build\classes de.marcelhuber.referenzprojektjavase7.controller.GrundschulVerwaltungStartMVC]
-java -cp .\dist\lib\*;build\classes de.marcelhuber.referenzprojektjavase7.controller.GrundschulVerwaltungStartMVC
+rem [java -cp .\dist\lib\*;build\classes de.marcelhuber.referenzprojektjavase7.controller.GrundschulVerwaltungStartMVC]
+java -cp .\dist\lib\*;.\dist\* de.marcelhuber.referenzprojektjavase7.controller.GrundschulVerwaltungStartMVC
 cmd
 rem [pause]
